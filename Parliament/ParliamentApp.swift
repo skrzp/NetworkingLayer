3bef528d43d4f8d874627d70a26fd448b413013a
pskrzypek <91827235+SkrzypekP@users.noreply.github.com>
2024-04-02 22:13:17 +0200

    Initial Commit

---
+++ Parliament/ParliamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParliamentApp.swift
+//  Parliament
+//
+//  Created by Patryk on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParliamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
